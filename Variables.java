1081362879981effb73b2e3c671753dbc17dba7a
bello yusuf <belloyusuf1997@gmail.com>
2024-08-03 09:24:44 +0100

    Learning variables.

---
+++ Variables.java
@@ -0,0 +1,18 @@
+public class Variables{
+    public static void main(String[] args){
+        String Name = "Bello";
+        char Letter = 'B';
+        final int Numbers = 4321;
+        // Numbers = 1234;
+        boolean status = true;
+        float  discout = 10.20f;
+
+
+
+        System.out.println("Hello " + Name);
+        System.out.println(Letter);
+        System.out.println(Numbers);
+        System.out.println(status);
+        System.out.println(discout);
+    }
+}
