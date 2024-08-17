012bf54f8a3b9f64e361ce618e97746d254d318d
bello yusuf <belloyusuf1997@gmail.com>
2024-08-17 11:36:53 +0100

    Done with loop

---
+++ loops.java
@@ -0,0 +1,21 @@
+public class loops {
+    public static void main(String[] args){
+        // while and do while loop
+
+        int num = 0;
+        // ======= uncomment it ==========
+        // while (num < 1){
+        //     System.out.println(num);
+        //     num ++;
+
+        // }
+
+
+        // do while 
+        do {
+            System.out.println(num);
+            num ++;
+        }
+        while(num < 2);
+    }
+}
