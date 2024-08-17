b3bdcf203b5d073fde3e115a03f40a644577afc4
bello yusuf <belloyusuf1997@gmail.com>
2024-08-17 11:37:23 +0100

    Done with it.

---
+++ Forloops.java
@@ -0,0 +1,13 @@
+public class Forloops {
+    public static void main(String[] args){
+        // for loop
+        for(int i = 0; i <= 10; i++ ){
+            System.out.println(i);
+        }
+        System.out.println("End of for loop Starting printing even numbers");
+        // print even number between 20
+        for (int i = 0; i <= 20; i = i + 2){
+            System.out.println(i);
+        }
+    }
+}
